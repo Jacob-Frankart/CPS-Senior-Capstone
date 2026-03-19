b35c54e05dc172579a0a2d6e958edae7b1ad5cca
Jacob Frankart <j.m.frankart@gmail.com>
2026-03-18 22:47:06 -0400

    Beginning Imports

---
+++ scikit-learnTesting.py
@@ -0,0 +1,15 @@
+#Libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyploy as plt
+import sklearn
+from sklearn.neural_network import MLPClassifier
+from sklearn.neural_network import MLPRegressor
+
+#Modules
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+from sklearn.metrics import r2_score
+
+print("Hello World")
